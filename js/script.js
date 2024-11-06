09432cbab129e89a202896593820d6f1b2b93451
luana4516 <luanaesther893@gmail.com>
2024-11-05 21:55:08 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Este script puede ser utilizado para agregar interactividad a tu página
+document.addEventListener('DOMContentLoaded', function() {
+    // Puedes agregar funcionalidades como botones, validaciones de formularios, etc.
+    console.log('El DOM ha sido completamente cargado');
+});
