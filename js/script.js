12ea638dcb93f81a8f5331f2009d386ce1f11a90
esther356 <payiesther934@gmail.com>
2024-11-05 22:17:22 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // script.js
 
-// Este script puede ser utilizado para agregar interactividad a tu página
+// Este script puede ser utilizado para agregar interactividad a la página
 document.addEventListener('DOMContentLoaded', function() {
     // Puedes agregar funcionalidades como botones, validaciones de formularios, etc.
     console.log('El DOM ha sido completamente cargado');
